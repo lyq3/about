757439e810d12a72e707a6946474ade400ada05f
罗云强 <99190092@qq.com>
2018-03-04 13:28:25 +0800

    图标

---
+++ static/styles/js/index.js
@@ -0,0 +1,34 @@
+layui.use(['util', 'laydate', 'layer'], function() {
+    var util = layui.util
+        , laydate = layui.laydate
+        , $ = layui.jquery
+        , layer = layui.layer;
+    //固定块
+    util.fixbar({
+        bar1: true
+        , bar2: true
+        , css: {right: 50, bottom: 100}
+        , bgcolor: '#393D49'
+        , click: function (type) {
+            if (type === 'bar1') {
+                layer.msg('icon是可以随便换的')
+            } else if (type === 'bar2') {
+                layer.msg('两个bar都可以设定是否开启')
+            }
+        }
+    });
+
+    //查看项目详情
+
+    $(".link-to").click(function(){
+        layer.open({
+            type: 2,
+            title: '项目详情',
+            shadeClose: true,
+            shade: false,
+            maxmin: true, //开启最大化最小化按钮
+            area: ['893px', '600px'],
+            content: 'http://fly.layui.com/'
+        });
+    })
+});
