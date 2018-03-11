7159e4a7df389ec2918e97f6331ee2b69c3976b6
罗云强 <99190092@qq.com>
2018-03-11 21:40:03 +0800

    项目描述

--- static/styles/js/index.js
+++ static/styles/js/index.js
@@ -31,4 +31,15 @@ layui.use(['util', 'laydate', 'layer'], function() {
             content: './page/ssglpt.html'
         });
     })
+
+    //未命名项目提示
+    $("#bestProject").click(function(){
+        layer.msg("该项目计划中，并未开始写.....<br/>我github还有其他一些没什么卵用的小项目",{
+            time: 20000, //20s后自动关闭
+            btn: ['去看看', '没兴趣'],
+            yes: function () {
+                window.open("https://github.com/lyq3");
+            }
+        });
+    })
 });
